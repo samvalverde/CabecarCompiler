10285781c829db913bcf5a7b80f0e9da07fef4a6
samuel valverde <savalverde@estudiantec.cr>
2024-09-04 00:30:55 -0600

    rutinas 1

--- ASM/Integer.asm
+++ ASM/Integer.asm
@@ -7,25 +7,24 @@
 ; Fecha de Entrega: 11/09/2024
 
 ; ###### Manual de Usuario ######
-
 ; Este programa es sumamente básico ya que recibe una entrada por terminal  
 ; y la imprime en pantalla. Esto funciona para los 6 tipos de datos que se
 ; manejarán en el proyecto de Compiladores e Intérpretes.
 
 ; ---------------------------------------------------------------
 datos segment
-    entero db ?
+    entero dw ?             ; Se amplía a 16 bits para manejar números grandes
     mensajeVacio1 db 'La entrada esta vacia.$'
     mensajeVacio2 db 'Por favor, ingrese un valor.$'
     acercaDe1 db 'Rutina de manejo para enteros.$'
     acercaDe2 db 'Manejo de entrada y salida.$'
     blank db 13, 10, '$'
-datos endS
+datos ends
 
 ; ---------------------------------------------------------------
 pila segment stack 'stack'
     dw 256 dup (?)
-pila endS
+pila ends
 
 Print Macro mensaje
     mov ah, 09h
@@ -39,45 +38,78 @@ codigo segment
     assume cs:codigo, ds:datos, ss:pila ;se asignan los segmentos
 
 obtenerEntrada proc near
-    ; Procesamiento de la entrada de un entero
-    mov ah, 01h
-    int 21h
-    sub al, '0'
-    mov entero, al
-    ret
-obtenerEntrada endP
+    ; Inicializar el acumulador a 0
+    mov cx, 0               ; CX almacenará el número
+    leerDigito:
+        mov ah, 01h         ; Leer un carácter
+        int 21h
+        cmp al, 0Dh         ; Comprobar si se presionó "Enter"
+        je finLectura
+        sub al, '0'         ; Convertir el carácter a valor numérico
+        mov bx, 10          ; BX = 10
+        mul bx              ; AX = AX * 10
+        add cx, ax          ; Sumar el dígito al acumulador
+        jmp leerDigito      ; Leer el siguiente dígito
+
+    finLectura:
+        mov entero, cx      ; Guardar el resultado en la variable "entero"
+        ret
+obtenerEntrada endp
 
 imprimirEntero proc near
-    ; Convierte y muestra el valor entero almacenado
-    mov al, entero
-    add al, '0'
+    ; Imprimir el valor entero almacenado
+    mov ax, entero          ; Cargar el valor de "entero" en AX
+    xor cx, cx              ; Inicializar CX a 0
+
+    cmp ax, 0               ; Comprobar si el número es 0
+    jne imprimirLoop        ; Si no es 0, continuar con la conversión
+    mov dl, '0'             ; Si es 0, mostrarlo directamente
     mov ah, 02h
     int 21h
-    ret
-imprimirEntero endP
+    jmp finImpresion
+
+    imprimirLoop:
+        mov bx, 10
+        xor dx, dx          ; DX:AX = AX / 10
+        div bx              ; Dividir AX por 10, resultado en AX, residuo en DX
+        add dl, '0'         ; Convertir el residuo en carácter
+        push dx             ; Guardar el dígito en la pila
+        inc cx              ; Incrementar el contador de dígitos
+        test ax, ax         ; Comprobar si AX es 0
+        jne imprimirLoop    ; Si no es 0, repetir
+
+    imprimirStack:
+        pop dx              ; Recuperar el dígito desde la pila
+        mov ah, 02h         ; Imprimir el dígito
+        int 21h
+        loop imprimirStack  ; Repetir para todos los dígitos
+
+    finImpresion:
+        ret
+imprimirEntero endp
 
 entradaVacia proc near
     Print mensajeVacio1
     Print mensajeVacio2
     ret
-entradaVacia endP
+entradaVacia endp
 
 finRutina proc near
     mov ax, 4C00h
     int 21h
     ret
-finRutina endP
+finRutina endp
 
 blankspace proc near
     Print blank
     ret
-blankspace endP
+blankspace endp
 
 prntAcercaDe proc near
     Print acercaDe1
     Print acercaDe2
     ret
-prntAcercaDe endP
+prntAcercaDe endp
 
 main:
     mov ax, ds
@@ -99,5 +131,5 @@ main:
     ;   ------------------------------------------------------------------
     call finRutina
 
-codigo endS
+codigo ends
 end main
--- ASM/Roman.asm
+++ ASM/Roman.asm
@@ -1,4 +1,4 @@
-; Proyecto - Rutinas de Números Romanos
+; Proyecto - Etapa 0: Rutinas de Números Romanos
 ; Samuel Valverde A.
 ; Erick Kauffmann P.
 ; IC5701 - Compiladores e Intérpretes, GR 2
@@ -14,17 +14,25 @@
 
 ; ---------------------------------------------------------------
 datos segment
-    random db ?
-    msgAcercaDe db 'Rutina de manejo para valores random (aleatorio).$'
-    msgGenerado db 'Numero aleatorio generado: $'
-    blank db 13, 10, '$'
+    ; Buffer para almacenar el número romano (máximo 19 caracteres + '$')
+    numerosRomanos db 20 dup('$') 
+    ; Mensajes de información
+    mensajeVacio1 db 'La entrada est',160,' vac',161,'a.$'
+    mensajeVacio2 db 'Por favor, ingrese un n',163,'mero romano.'13, 10, '$'
+    acercaDe1 db 'Rutina de manejo para n',163,'meros romanos.'13, 10, '$'
+    acercaDe2 db 'Manejo de entrada y salida.$'
+    mensajeEntrada db 'Ingrese un n',163,'mero romano: $'
+    mensajeSalida db 'El n',163,'mero romano ingresado es: $'
+    blank db 13, 10, '$' ; Línea en blanco
 datos endS
 
 ; ---------------------------------------------------------------
 pila segment stack 'stack'
-    dw 256 dup (?)
+    dw 256 dup (?) ; Reserva espacio para la pila
 pila endS
 
+; ---------------------------------------------------------------
+; Macro para imprimir mensajes
 Print Macro mensaje
     mov ah, 09h
     lea dx, mensaje
@@ -34,45 +42,90 @@ EndM
 ; ---------------------------------------------------------------
 codigo segment
 
-    assume cs:codigo, ds:datos, ss:pila ;se asignan los segmentos
+    assume cs:codigo, ds:datos, ss:pila ; Asignación de segmentos
 
-generarRandom proc near
-    ; Genera un número aleatorio y lo guarda en random
-    mov ah, 2Ch             ; Llamada a la función para obtener el reloj del sistema
-    int 21h                 ; Interrupt 21h, función 2Ch - hora del sistema
-    mov al, dh              ; Usa el valor de los segundos para generar el aleatorio
-    mov random, al          ; Guarda el número generado en random
+; Procedimiento para imprimir el mensaje "Acerca de"
+prntAcercaDe proc near
+    Print acercaDe1
+    Print acercaDe2
     ret
-generarRandom endP
+prntAcercaDe endP
 
-imprimirRandom proc near
-    ; Convierte y muestra el valor random generado
-    mov al, random
-    ; Convertir el valor a un carácter imprimible (0-9)
-    and al, 0Fh             ; Limita el valor entre 0-15 (para evitar caracteres no imprimibles)
-    add al, '0'             ; Convierte a carácter numérico
-    mov ah, 02h
+; Procedimiento para manejar entradas vacías
+entradaVacia proc near
+    Print mensajeVacio1
+    Print mensajeVacio2
+    ret
+entradaVacia endP
+
+; Procedimiento para obtener la entrada del usuario
+obtenerEntradaNumerosRomanos proc near
+    ; Mostrar mensaje de solicitud de entrada
+    Print mensajeEntrada
+
+    ; Inicializar puntero al buffer y contador
+    lea si, numerosRomanos
+    mov cx, 0
+
+    leerCaracter:
+        ; Leer un carácter de la entrada estándar
+        mov ah, 01h
+        int 21h
+        cmp al, 0Dh           ; Comprobar si es "Enter" (código ASCII 13)
+        je finLectura         ; Si es "Enter", terminar la lectura
+
+        ; Almacenar el carácter en el buffer
+        mov [si], al
+        inc si                ; Incrementar el puntero
+        inc cx                ; Incrementar el contador de caracteres leídos
+        cmp cx, 19            ; Verificar que no se exceda el buffer
+        jae finLectura        ; Si se excede, terminar la lectura
+        jmp leerCaracter      ; Repetir para el siguiente carácter
+
+    empty:
+        ; Mostrar mensaje de entrada vacía
+        call entradaVacia
+        ret
+    
+    finLectura:
+        ; Añadir el carácter de fin de cadena '$'
+        mov byte ptr [si], '$'
+        ; Verificar si la entrada está vacía
+        lea si, numerosRomanos
+        mov al, [si]
+        cmp al, '$'           ; Comprobar si el primer carácter es '$' (sin entrada)
+        je empty       ; Si es '$', mostrar mensaje de entrada vacía
+        ret
+obtenerEntradaNumerosRomanos endP
+
+; Procedimiento para imprimir el número romano almacenado
+imprimirNumerosRomanos proc near
+    ; Mostrar mensaje de salida
+    Print mensajeSalida
+
+    ; Imprimir el string almacenado en el buffer
+    lea dx, numerosRomanos
+    mov ah, 09h
     int 21h
     ret
-imprimirRandom endP
+imprimirNumerosRomanos endP
 
+; Procedimiento para imprimir una línea en blanco
+blankspace proc near
+    Print blank
+    ret
+blankspace endP
+
+; Procedimiento para finalizar el programa
 finRutina proc near
     mov ax, 4C00h
     int 21h
     ret
 finRutina endP
 
-blankspace proc near
-    Print blank
-    ret
-blankspace endP
-
-prntAcercaDe proc near
-    Print msgAcercaDe
-    ret
-prntAcercaDe endP
-
+; Procedimiento principal del programa
 main:
+    ; Inicialización de segmentos
     mov ax, ds
     mov es, ax
 
@@ -81,17 +134,19 @@ main:
 
     mov ax, datos
     mov ds, ax
-    ;   ------------------------------------------------------------------
+
+    ; ------------------------------------------------------------------
     call prntAcercaDe
-    ;   ------------------------------------------------------------------
+    ; ------------------------------------------------------------------
     call blankspace
-    ;   ------------------------------------------------------------------
-    call generarRandom
-    ;   ------------------------------------------------------------------
-    Print msgGenerado
-    call imprimirRandom
-    ;   ------------------------------------------------------------------
+    ; ------------------------------------------------------------------
+    call obtenerEntradaNumerosRomanos
+    ; ------------------------------------------------------------------
+    call imprimirNumerosRomanos
+    ; ------------------------------------------------------------------
     call finRutina
 
 codigo endS
 end main
+
+; ---------------------------------------------------------------
